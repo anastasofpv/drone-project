1611aa90588cafa174e7022c64139f9e478a8b03
anastasofpv <anastasofpv@gmail.com>
2022-06-22 19:13:51 +0300

    server v1

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controller/controller.js');
+
+router.get("/getData", controller.mainPage)
+
+
+module.exports = router;
